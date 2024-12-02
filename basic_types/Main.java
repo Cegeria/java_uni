3b77b23462fb34837b3c7daccaa6bc3daeea6130
Cegeria <sergeyme601@gmail.com>
2024-12-02 12:08:16 +0300

    2

---
+++ basic_types/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Имя "Сергей" в формате Unicode
+        System.out.println("\u0421\u0435\u0440\u0433\u0435\u0439");
+    }
+}
